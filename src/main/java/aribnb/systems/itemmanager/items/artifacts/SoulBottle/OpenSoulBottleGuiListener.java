7acfae6763a401c68eea3c4db0c2b9c6a23ba81d
Maksasj <radioboos@gmail.com>
2022-06-20 19:47:04 +0300

    OpenSoulBottleGuiListener fix, closes #2

--- src/main/java/aribnb/systems/itemmanager/items/artifacts/SoulBottle/OpenSoulBottleGuiListener.java
+++ src/main/java/aribnb/systems/itemmanager/items/artifacts/SoulBottle/OpenSoulBottleGuiListener.java
@@ -28,111 +28,115 @@ public class OpenSoulBottleGuiListener implements Listener {
             return;
         }
 
-        if(event.getClickedInventory().getHolder() instanceof SoulBottleGui) {
-            event.setCancelled(true);
-
-            Player player = (Player) event.getWhoClicked();
-            if(event.getCurrentItem() == null) {
-                return;
-            }
-
-            if(event.getCurrentItem().getType() == Material.BUCKET) {
+        if(event.getClickedInventory() != null) { // Issue "InventoryGui click error #2"
+            if(event.getClickedInventory().getHolder() instanceof SoulBottleGui) {
                 event.setCancelled(true);
 
-                Double player_souls = SoulSystem.getPlayerSouls(player);
-
-                if (player.getInventory().getItemInMainHand().getType() == Material.POTION) {
-                    AribnbNbtFormater nbt = new AribnbNbtFormater(player.getInventory().getItemInMainHand());
-
-                    Double bottle_max_souls = nbt.getDoubleField("aribnb_soulbottle_max_capacity");
-                    Double bottle_souls = nbt.getDoubleField("aribnb_soulbottle_capacity");
-                    Double updated_bottle_souls = 0.0;
-
-                    if(bottle_souls + player_souls < bottle_max_souls) {
-                        updated_bottle_souls = bottle_souls + player_souls;
-                        player_souls = 0.0;
-                    } else {
-                        updated_bottle_souls = bottle_max_souls;
-                        player_souls = (bottle_max_souls - bottle_souls);
-                    }
-
-                    if(nbt.hasStringField("aribnb_artifact")) {
-                        if(nbt.getStringField("aribnb_artifact").equals("aribnb_soulbottle")) {
-                            ItemStack item = player.getInventory().getItemInMainHand();
-                            ItemMeta meta = item.getItemMeta();
-
-                            ItemLoreBuilder lorebuilder = new ItemLoreBuilder();
-                            List<String> ab_lore = new ArrayList<>();;
-                            ab_lore.add("Store and accumulate");
-                            ab_lore.add("gained souls");
-
-                            lorebuilder.setItemType("ARTIFACT");
-                            lorebuilder.addCustomInfo("§bSouls stored: "+updated_bottle_souls+"/"+bottle_max_souls);
-                            lorebuilder.setRarity(Rarities.EPIC);
-
-                            lorebuilder.addItemAbilitieLore(new ItemAbilitiesLore(ab_lore, AbilitieLoreType.CLICK));
-                            meta.setLore(lorebuilder.buildLore());
-
-                            PersistentDataContainer data = meta.getPersistentDataContainer();
-                            data.set(new NamespacedKey(Aribnb.getPlugin(), "aribnb_soulbottle_capacity"), PersistentDataType.DOUBLE, updated_bottle_souls);
-
-                            item.setItemMeta(meta);
-
-                            player.getInventory().setItemInMainHand(item);
-                        }
-                    }
+                Player player = (Player) event.getWhoClicked();
+                if (event.getCurrentItem() == null) {
+                    return;
                 }
 
-                SoulSystem.setSoulToPlayer(player, player_souls);
-                player.closeInventory();
-            } else if(event.getCurrentItem().getType() == Material.WATER_BUCKET) {
-                event.setCancelled(true);
+                if (event.getCurrentItem().getType() == Material.BUCKET) {
+                    event.setCancelled(true);
 
-                Double player_souls = SoulSystem.getPlayerSouls(player);
+                    Double player_souls = SoulSystem.getPlayerSouls(player);
 
-                if (player.getInventory().getItemInMainHand().getType() == Material.POTION) {
-                    AribnbNbtFormater nbt = new AribnbNbtFormater(player.getInventory().getItemInMainHand());
+                    if (player.getInventory().getItemInMainHand().getType() == Material.POTION) {
+                        AribnbNbtFormater nbt = new AribnbNbtFormater(player.getInventory().getItemInMainHand());
 
-                    Double player_max_souls = SoulSystem.getPlayerMaxSouls(player);
-                    Double bottle_max_souls = nbt.getDoubleField("aribnb_soulbottle_max_capacity");
-                    Double bottle_souls = nbt.getDoubleField("aribnb_soulbottle_capacity");
+                        Double bottle_max_souls = nbt.getDoubleField("aribnb_soulbottle_max_capacity");
+                        Double bottle_souls = nbt.getDoubleField("aribnb_soulbottle_capacity");
+                        Double updated_bottle_souls = 0.0;
 
-                    if(bottle_souls + player_souls < player_max_souls) {
-                        player_souls += bottle_souls;
-                    } else {
-                        bottle_souls -= (player_max_souls - player_souls);
-                        player_souls = player_max_souls;
-                    }
+                        if (bottle_souls + player_souls < bottle_max_souls) {
+                            updated_bottle_souls = bottle_souls + player_souls;
+                            player_souls = 0.0;
+                        } else {
+                            updated_bottle_souls = bottle_max_souls;
+                            player_souls = (bottle_max_souls - bottle_souls);
+                        }
 
-                    if(nbt.hasStringField("aribnb_artifact")) {
-                        if(nbt.getStringField("aribnb_artifact").equals("aribnb_soulbottle")) {
-                            ItemStack item = player.getInventory().getItemInMainHand();
-                            ItemMeta meta = item.getItemMeta();
+                        if (nbt.hasStringField("aribnb_artifact")) {
+                            if (nbt.getStringField("aribnb_artifact").equals("aribnb_soulbottle")) {
+                                ItemStack item = player.getInventory().getItemInMainHand();
+                                ItemMeta meta = item.getItemMeta();
 
-                            ItemLoreBuilder lorebuilder = new ItemLoreBuilder();
-                            List<String> ab_lore = new ArrayList<>();;
-                            ab_lore.add("Store and accumulate");
-                            ab_lore.add("gained souls");
+                                ItemLoreBuilder lorebuilder = new ItemLoreBuilder();
+                                List<String> ab_lore = new ArrayList<>();
+                                ;
+                                ab_lore.add("Store and accumulate");
+                                ab_lore.add("gained souls");
 
-                            lorebuilder.setItemType("ARTIFACT");
-                            lorebuilder.addCustomInfo("§bSouls stored: "+bottle_souls+"/"+bottle_max_souls);
-                            lorebuilder.setRarity(Rarities.EPIC);
+                                lorebuilder.setItemType("ARTIFACT");
+                                lorebuilder.addCustomInfo("§bSouls stored: " + updated_bottle_souls + "/" + bottle_max_souls);
+                                lorebuilder.setRarity(Rarities.EPIC);
 
-                            lorebuilder.addItemAbilitieLore(new ItemAbilitiesLore(ab_lore, AbilitieLoreType.CLICK));
-                            meta.setLore(lorebuilder.buildLore());
+                                lorebuilder.addItemAbilitieLore(new ItemAbilitiesLore(ab_lore, AbilitieLoreType.CLICK));
+                                meta.setLore(lorebuilder.buildLore());
 
-                            PersistentDataContainer data = meta.getPersistentDataContainer();
-                            data.set(new NamespacedKey(Aribnb.getPlugin(), "aribnb_soulbottle_capacity"), PersistentDataType.DOUBLE, bottle_souls);
+                                PersistentDataContainer data = meta.getPersistentDataContainer();
+                                data.set(new NamespacedKey(Aribnb.getPlugin(), "aribnb_soulbottle_capacity"), PersistentDataType.DOUBLE, updated_bottle_souls);
 
-                            item.setItemMeta(meta);
+                                item.setItemMeta(meta);
 
-                            player.getInventory().setItemInMainHand(item);
+                                player.getInventory().setItemInMainHand(item);
+                            }
                         }
                     }
-                }
 
-                SoulSystem.setSoulToPlayer(player, player_souls);
-                player.closeInventory();
+                    SoulSystem.setSoulToPlayer(player, player_souls);
+                    player.closeInventory();
+                } else if (event.getCurrentItem().getType() == Material.WATER_BUCKET) {
+                    event.setCancelled(true);
+
+                    Double player_souls = SoulSystem.getPlayerSouls(player);
+
+                    if (player.getInventory().getItemInMainHand().getType() == Material.POTION) {
+                        AribnbNbtFormater nbt = new AribnbNbtFormater(player.getInventory().getItemInMainHand());
+
+                        Double player_max_souls = SoulSystem.getPlayerMaxSouls(player);
+                        Double bottle_max_souls = nbt.getDoubleField("aribnb_soulbottle_max_capacity");
+                        Double bottle_souls = nbt.getDoubleField("aribnb_soulbottle_capacity");
+
+                        if (bottle_souls + player_souls < player_max_souls) {
+                            player_souls += bottle_souls;
+                        } else {
+                            bottle_souls -= (player_max_souls - player_souls);
+                            player_souls = player_max_souls;
+                        }
+
+                        if (nbt.hasStringField("aribnb_artifact")) {
+                            if (nbt.getStringField("aribnb_artifact").equals("aribnb_soulbottle")) {
+                                ItemStack item = player.getInventory().getItemInMainHand();
+                                ItemMeta meta = item.getItemMeta();
+
+                                ItemLoreBuilder lorebuilder = new ItemLoreBuilder();
+                                List<String> ab_lore = new ArrayList<>();
+                                ;
+                                ab_lore.add("Store and accumulate");
+                                ab_lore.add("gained souls");
+
+                                lorebuilder.setItemType("ARTIFACT");
+                                lorebuilder.addCustomInfo("§bSouls stored: " + bottle_souls + "/" + bottle_max_souls);
+                                lorebuilder.setRarity(Rarities.EPIC);
+
+                                lorebuilder.addItemAbilitieLore(new ItemAbilitiesLore(ab_lore, AbilitieLoreType.CLICK));
+                                meta.setLore(lorebuilder.buildLore());
+
+                                PersistentDataContainer data = meta.getPersistentDataContainer();
+                                data.set(new NamespacedKey(Aribnb.getPlugin(), "aribnb_soulbottle_capacity"), PersistentDataType.DOUBLE, bottle_souls);
+
+                                item.setItemMeta(meta);
+
+                                player.getInventory().setItemInMainHand(item);
+                            }
+                        }
+                    }
+
+                    SoulSystem.setSoulToPlayer(player, player_souls);
+                    player.closeInventory();
+                }
             }
         }
     }
